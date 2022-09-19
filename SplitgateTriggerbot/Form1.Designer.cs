c837145bad03b111c28cfc460899b701ef6fac25
Gaffer <95588473+Gaffer1337@users.noreply.github.com>
2022-09-19 22:22:52 +0100

    Add files via upload

---
+++ SplitgateTriggerbot/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace SplitgateTriggerbot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gunaElipsePanel1 = new Guna.UI.WinForms.GunaElipsePanel();
+            this.gunaLabel1 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaCheckBox1 = new Guna.UI.WinForms.GunaCheckBox();
+            this.gunaCheckBox2 = new Guna.UI.WinForms.GunaCheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gunaElipsePanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gunaElipsePanel1
+            // 
+            this.gunaElipsePanel1.BackColor = System.Drawing.Color.Transparent;
+            this.gunaElipsePanel1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(46)))), ((int)(((byte)(46)))));
+            this.gunaElipsePanel1.Controls.Add(this.label1);
+            this.gunaElipsePanel1.Controls.Add(this.gunaCheckBox2);
+            this.gunaElipsePanel1.Controls.Add(this.gunaCheckBox1);
+            this.gunaElipsePanel1.Controls.Add(this.gunaLabel1);
+            this.gunaElipsePanel1.Location = new System.Drawing.Point(1, 2);
+            this.gunaElipsePanel1.Name = "gunaElipsePanel1";
+            this.gunaElipsePanel1.Radius = 10;
+            this.gunaElipsePanel1.Size = new System.Drawing.Size(223, 256);
+            this.gunaElipsePanel1.TabIndex = 0;
+            // 
+            // gunaLabel1
+            // 
+            this.gunaLabel1.AutoSize = true;
+            this.gunaLabel1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.gunaLabel1.Location = new System.Drawing.Point(11, 7);
+            this.gunaLabel1.Name = "gunaLabel1";
+            this.gunaLabel1.Size = new System.Drawing.Size(192, 25);
+            this.gunaLabel1.TabIndex = 0;
+            this.gunaLabel1.Text = "Splitgate TriggerBot";
+            // 
+            // gunaCheckBox1
+            // 
+            this.gunaCheckBox1.BaseColor = System.Drawing.Color.White;
+            this.gunaCheckBox1.CheckedOffColor = System.Drawing.Color.Gray;
+            this.gunaCheckBox1.CheckedOnColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.gunaCheckBox1.FillColor = System.Drawing.Color.White;
+            this.gunaCheckBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaCheckBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.gunaCheckBox1.Location = new System.Drawing.Point(16, 53);
+            this.gunaCheckBox1.Name = "gunaCheckBox1";
+            this.gunaCheckBox1.Size = new System.Drawing.Size(83, 20);
+            this.gunaCheckBox1.TabIndex = 1;
+            this.gunaCheckBox1.Text = "TriggerBot";
+            this.gunaCheckBox1.CheckedChanged += new System.EventHandler(this.gunaCheckBox1_CheckedChanged);
+            // 
+            // gunaCheckBox2
+            // 
+            this.gunaCheckBox2.BaseColor = System.Drawing.Color.White;
+            this.gunaCheckBox2.CheckedOffColor = System.Drawing.Color.Gray;
+            this.gunaCheckBox2.CheckedOnColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.gunaCheckBox2.FillColor = System.Drawing.Color.White;
+            this.gunaCheckBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaCheckBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.gunaCheckBox2.Location = new System.Drawing.Point(16, 79);
+            this.gunaCheckBox2.Name = "gunaCheckBox2";
+            this.gunaCheckBox2.Size = new System.Drawing.Size(108, 20);
+            this.gunaCheckBox2.TabIndex = 2;
+            this.gunaCheckBox2.Text = "Recoil Control";
+            this.gunaCheckBox2.CheckedChanged += new System.EventHandler(this.gunaCheckBox2_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 179);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightPink;
+            this.ClientSize = new System.Drawing.Size(248, 270);
+            this.Controls.Add(this.gunaElipsePanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "2E2v1HCKZ3CBMcIxY4i7";
+            this.TransparencyKey = System.Drawing.Color.LightPink;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gunaElipsePanel1.ResumeLayout(false);
+            this.gunaElipsePanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI.WinForms.GunaElipsePanel gunaElipsePanel1;
+        private Guna.UI.WinForms.GunaCheckBox gunaCheckBox2;
+        private Guna.UI.WinForms.GunaCheckBox gunaCheckBox1;
+        private Guna.UI.WinForms.GunaLabel gunaLabel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SplitgateTriggerbot/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using AutoItX3Lib;
+
+namespace SplitgateTriggerbot
+{
+    public partial class Form1 : Form
+    {
+
+        AutoItX3 au3 = new AutoItX3();
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string green = "65280";
+            string red = "16711680";
+            string shotgungreen = "65024";
+            string shotgunred = "15860226";
+            while (true)
+            {
+                object pix1 = au3.PixelGetColor(1280, 720);
+                object pix3 = au3.PixelGetColor(1280, 672);
+                object pix4 = au3.PixelGetColor(1326, 749);
+                object pix5 = au3.PixelGetColor(1297, 719);
+                object pix6 = au3.PixelGetColor(1249, 674);
+                object pix7 = au3.PixelGetColor(1318, 681);
+                object pix8 = au3.PixelGetColor(1238, 756);
+                object pix9 = au3.PixelGetColor(1262, 720);
+                object pix10 = au3.PixelGetColor(1280, 715);
+                object pix11 = au3.PixelGetColor(1280, 720);
+                object pix12 = au3.PixelGetColor(1280, 720);
+                object pix13 = au3.PixelGetColor(1280, 720);
+                object pix14 = au3.PixelGetColor(1280, 720);
+                var smg = pix1.ToString();
+                var carbine = pix11.ToString();
+                var battle = pix12.ToString();
+                var bfb = pix13.ToString();
+                var rocket = pix14.ToString();
+                var plasma = pix3.ToString();
+                var shotgun = pix4.ToString();
+                var shotgun2 = pix6.ToString();
+                var shotgun3 = pix7.ToString();
+                var shotgun4 = pix8.ToString();
+                var pistol = pix5.ToString();
+                var pistol2 = pix9.ToString();
+                var pistol3 = pix10.ToString();
+                if (smg == "16711680" || pistol == "14550020" || pistol2 == "14615556" || pistol3 == "15532803" || plasma == "16711680")//auto
+                {
+                    au3.MouseDown("LEFT");
+                }
+                else if(shotgun == "16122113" || shotgun2 == "16121856" || shotgun3 == "15729154" || shotgun4 == "16711680" || carbine == "16711680" || battle == "16711680" || bfb == "16711680" || rocket == "16711680")//semi auto
+                {
+                    au3.MouseClick("LEFT");
+                }
+                else
+                {
+
+                }
+            }
+        }
+
+        private void leftuponce()
+        {
+            au3.MouseUp("LEFT");
+            
+        }
+
+        private void gunaCheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gunaCheckBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
